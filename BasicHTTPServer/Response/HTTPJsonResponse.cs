83f33e372fe556a805d20dc4a18c5e42a9c6fe9d
Noah Forberich <noah@forberich.net>
2021-06-19 13:22:16 +0200

    Add Json and HTML Response Classes

---
+++ BasicHTTPServer/Response/HTTPJsonResponse.cs
@@ -0,0 +1,37 @@
+﻿// // This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using System.Text;
+
+namespace BasicHTTPServer.Response
+{
+    /// <summary>
+    /// Basic Response Returning JSON to the Client.
+    /// </summary>
+    public class HTTPJsonResponse : HTTPResponse
+    {
+        private string _responseBody;
+
+        public HTTPJsonResponse(string body)
+        {
+            _responseBody = body;
+        }
+
+        /// <summary>
+        /// This function assembles Head, Headers and Text Body.
+        /// </summary>
+        /// <returns>Bytes Ready to be send over the network.</returns>
+        public override byte[] ProcessResponse()
+        {
+            var responseHttp = new StringBuilder();
+            SetStatusCode(200);
+            responseHttp.Append(ProcessHead());
+            Headers.Add("Content-Type", "text/json");
+            responseHttp.Append(ProcessHeader());
+            responseHttp.Append('\n');
+            responseHttp.Append(_responseBody);
+            return Encoding.UTF8.GetBytes(responseHttp.ToString());
+        }
+
+    }
+}
